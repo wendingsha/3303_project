5188a047ac62211db7fdfff830e40b1d77ba82d0
wendingsha <wendingsha@cmail.carleton.ca>
2025-03-08 18:19:41 -0500

    Update floor.cpp

--- iteration3/floor.cpp
+++ iteration3/floor.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <thread>
+#include <chrono>
 
-#define SCHEDULER_IP "127.0.0.1"
 #define SCHEDULER_PORT 9001
 
 void floorFunction() {
@@ -14,6 +15,8 @@ void floorFunction() {
         std::cerr << "Error: Unable to open input.txt. Exiting floorFunction." << std::endl;
         return;
     }
+
+    std::cout<<"Floor: Reading requests from input.txt..."<<std::endl;
 
     int floor, destination;
     while (inputFile >> floor >> destination) {
@@ -26,10 +29,14 @@ void floorFunction() {
         try {
             sendSocket.send(sendPacket);
             std::cout << "Floor: Sent request " << request << " to Scheduler" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
         } catch (const std::runtime_error& e) {
             std::cerr << "Error sending request: " << e.what() << std::endl;
         }
     }
+
+    std::cout << "Floor: All requests sent. Waiting before exit..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 }
 
 int main() {
