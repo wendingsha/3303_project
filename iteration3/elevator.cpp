6d0d9fe3c5a25f0e87d7178e95f841804147ba90
wendingsha <wendingsha@cmail.carleton.ca>
2025-03-08 15:12:54 -0500

    Update elevator.cpp

--- iteration3/elevator.cpp
+++ iteration3/elevator.cpp
@@ -4,27 +4,20 @@
 #include <sstream>
 #include <thread>
 #include <chrono>
-#include <mutex>
-#include <condition_variable>
 
 #define ELEVATOR_PORT 9002
-#define SCHEDULER_IP "127.0.0.1"
 #define SCHEDULER_PORT 9001
 
 enum class ElevatorState { IDLE, MOVING, STOPPING, DOOR_OPEN, DOOR_CLOSED };
-
-std::mutex mtx;
-std::condition_variable cv;
-bool systemActive = true;
 
 void elevatorFunction(int elevatorId) {
     DatagramSocket receiveSocket(ELEVATOR_PORT);
     DatagramSocket sendSocket;
     ElevatorState state = ElevatorState::IDLE;
 
-    std::cout << "Elevator: Listening on port " << ELEVATOR_PORT << "..." << std::endl;
+    std::cout << "Elevator " << elevatorId << " Listening on port " << ELEVATOR_PORT << "..." << std::endl;
 
-    while (systemActive) {
+    while (true) {
         std::vector<uint8_t> data(100);
         DatagramPacket receivePacket(data, data.size());
 
@@ -40,7 +33,7 @@ void elevatorFunction(int elevatorId) {
         int receivedElevatorId, pickupFloor, destinationFloor;
         iss >> receivedElevatorId >> pickupFloor >> destinationFloor;
 
-        if (receivedElevatorId != elevatorId) continue; 
+        if (receivedElevatorId != elevatorId) continue;
 
         std::cout << "Elevator " << elevatorId << " moving from " << pickupFloor << " to " << destinationFloor << std::endl;
 
@@ -48,29 +41,16 @@ void elevatorFunction(int elevatorId) {
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
         state = ElevatorState::STOPPING;
-        std::cout << "Elevator " << elevatorId << " stopping at Floor " << pickupFloor << std::endl;
-        
         state = ElevatorState::DOOR_OPEN;
-        std::cout << "Elevator " << elevatorId << " doors opening at Floor " << pickupFloor << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
         state = ElevatorState::DOOR_CLOSED;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-        state = ElevatorState::MOVING;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-        state = ElevatorState::STOPPING;
-        std::cout << "Elevator " << elevatorId << " stopping at Floor " << destinationFloor << std::endl;
-
-        state = ElevatorState::DOOR_OPEN;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-        std::string updateMsg = std::to_string(elevatorId) + " IDLE";
+        std::string updateMsg = std::to_string(elevatorId) + " " + std::to_string(destinationFloor) + " IDLE";
         std::vector<uint8_t> out(updateMsg.begin(), updateMsg.end());
         DatagramPacket sendPacket(out, out.size(), InetAddress::getLocalHost(), SCHEDULER_PORT);
         sendSocket.send(sendPacket);
 
         std::cout << "Elevator " << elevatorId << " is now idle." << std::endl;
+        state = ElevatorState::IDLE;
     }
 }
