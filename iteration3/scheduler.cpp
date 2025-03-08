3d617b6b5b63f548190dd938f0c0a6a4e7187a43
wendingsha <wendingsha@cmail.carleton.ca>
2025-03-07 21:54:15 -0500

    add state machine

--- iteration3/scheduler.cpp
+++ iteration3/scheduler.cpp
@@ -16,33 +16,54 @@ extern std::vector<int> elevatorPositions;
 extern bool systemActive;
 std::vector<bool> elevatorBusy(1, false); // Single elevator
 
+enum class SchedulerState {
+    IDLE,
+    WAITING_FOR_REQUEST,
+    WAITING_FOR_ELEVATOR,
+    ASSIGNING_REQUEST
+};
+
 void schedulerFunction() {
+    SchedulerState state = SchedulerState::IDLE;
+
     while (systemActive) {
         std::unique_lock<std::mutex> lk(mtx);
 
-        if (schedulerQueue.empty()) {
-            cv.wait(lk, [] { return !schedulerQueue.empty(); });
+        switch (state) {
+            case SchedulerState::IDLE:
+                state = SchedulerState::WAITING_FOR_REQUEST;
+                break;
+
+            case SchedulerState::WAITING_FOR_REQUEST:
+                cv.wait(lk, [] { return !schedulerQueue.empty(); });
+                state = SchedulerState::WAITING_FOR_ELEVATOR;
+                break;
+
+            case SchedulerState::WAITING_FOR_ELEVATOR:
+                cv.wait(lk, [] { return !elevatorBusy[0]; });
+                state = SchedulerState::ASSIGNING_REQUEST;
+                break;
+
+            case SchedulerState::ASSIGNING_REQUEST: {
+                ElevatorMessage request = schedulerQueue.front();
+                schedulerQueue.pop();
+                lk.unlock();
+
+                int bestElevator = 0; // Only one elevator
+                request.assignedElevator = bestElevator;
+                elevatorBusy[bestElevator] = true;
+
+                {
+                    std::lock_guard<std::mutex> lock(mtx);
+                    elevatorQueue.push(request);
+                    std::cout << "Scheduler assigned Floor " << request.floorNumber
+                              << " to Elevator " << bestElevator << std::endl;
+                }
+
+                cv.notify_all();
+                state = SchedulerState::IDLE;
+                break;
+            }
         }
-
-        // Wait for elevator to be free before assigning a new request
-        cv.wait(lk, [] { return !elevatorBusy[0]; });
-
-        ElevatorMessage request = schedulerQueue.front();
-        schedulerQueue.pop();
-        lk.unlock();
-
-        int bestElevator = 0; // Only one elevator
-
-        request.assignedElevator = bestElevator;
-        elevatorBusy[bestElevator] = true; // Mark elevator as busy
-
-        {
-            std::lock_guard<std::mutex> lock(mtx);
-            elevatorQueue.push(request);
-            std::cout << "Scheduler assigned Floor " << request.floorNumber
-                      << " to Elevator " << bestElevator << std::endl;
-        }
-
-        cv.notify_all();
     }
 }
