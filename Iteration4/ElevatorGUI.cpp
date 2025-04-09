be1cd6ef3509dc99d716b0dda4760ae9965f3243
wendingsha <wendingsha@cmail.carleton.ca>
2025-04-08 21:49:19 -0400

    Update ElevatorGUI.cpp

--- Iteration4/ElevatorGUI.cpp
+++ Iteration4/ElevatorGUI.cpp
@@ -5,82 +5,63 @@
 #include "floor.cpp"
 #include "scheduler.cpp"
 #include "elevator.cpp"
+#include "shared.hpp"
 
-// Global variables shared across modules
 std::atomic<int> currentTime(0);
 std::mutex printMutex;
 bool systemActive = true;
 
-/**
- * Constructor for ElevatorGUI
- * Sets up the UI layout and connects button signals to their slots
- */
 ElevatorGUI::ElevatorGUI(QWidget *parent)
     : QMainWindow(parent) {
 
-    // Set up central widget and layout
     QWidget *central = new QWidget(this);
     QVBoxLayout *layout = new QVBoxLayout(central);
 
-    // Create buttons and log display
     startButton = new QPushButton("Start System");
-    requestButton = new QPushButton("Send Request");
+    statusTable = new QTableWidget(2, 5);
+    statusTable->setHorizontalHeaderLabels({"Elevator", "Current Floor", "Status", "Destination", "Fault"});
     logBox = new QTextEdit();
     logBox->setReadOnly(true);
 
-    // Add widgets to layout
     layout->addWidget(startButton);
-    layout->addWidget(requestButton);
+    layout->addWidget(statusTable);
     layout->addWidget(logBox);
     setCentralWidget(central);
 
-    // Connect button clicks to their respective slot functions
     connect(startButton, &QPushButton::clicked, this, &ElevatorGUI::handleStartSystem);
-    connect(requestButton, &QPushButton::clicked, this, &ElevatorGUI::handleSendRequest);
 
-    setWindowTitle("Elevator System GUI Controller");
-    resize(400, 300);
+    // Auto refresh every second
+    updateTimer = new QTimer(this);
+    connect(updateTimer, &QTimer::timeout, this, &ElevatorGUI::refreshStatusTable);
+    updateTimer->start(1000);
+
+    setWindowTitle("Elevator System Monitor");
+    resize(600, 400);
 }
 
-/**
- * Destructor for ElevatorGUI
- */
 ElevatorGUI::~ElevatorGUI() {}
 
-/**
- * Appends a message to the log window with timestamp
- */
 void ElevatorGUI::logMessage(const QString &msg) {
     QString timestamp = QDateTime::currentDateTime().toString("hh:mm:ss");
     logBox->append("[" + timestamp + "] " + msg);
 }
 
-/**
- * Starts the elevator system by launching each component in a separate thread
- */
 void ElevatorGUI::handleStartSystem() {
-    logMessage("Starting elevator system...");
+    logMessage("System starting...");
 
-    // Start Scheduler
     std::thread schedulerThread([]() {
         schedulerFunction();
     });
-
-    // Start two elevators
     std::thread elevator0([]() {
         elevatorFunction(0);
     });
-
     std::thread elevator1([]() {
         elevatorFunction(1);
     });
-
-    // Start floor simulation (reads from input.txt)
     std::thread floorThread([]() {
         floorFunction();
     });
 
-    // Detach all threads so they run independently
     schedulerThread.detach();
     elevator0.detach();
     elevator1.detach();
@@ -89,10 +70,14 @@ void ElevatorGUI::handleStartSystem() {
     logMessage("All components started.");
 }
 
-/**
- * Simulates sending a request (currently just logs a message)
- * You can extend this to manually trigger requests
- */
-void ElevatorGUI::handleSendRequest() {
-    logMessage("Simulated request (input.txt will be used by floorFunction).");
+void ElevatorGUI::refreshStatusTable() {
+    std::lock_guard<std::mutex> lock(statusMutex);
+    for (int i = 0; i < elevatorStatuses.size(); ++i) {
+        const ElevatorStatus &e = elevatorStatuses[i];
+        statusTable->setItem(i, 0, new QTableWidgetItem(QString::number(e.id)));
+        statusTable->setItem(i, 1, new QTableWidgetItem(QString::number(e.currentFloor)));
+        statusTable->setItem(i, 2, new QTableWidgetItem(QString::fromStdString(e.status)));
+        statusTable->setItem(i, 3, new QTableWidgetItem(e.destination == -1 ? "-" : QString::number(e.destination)));
+        statusTable->setItem(i, 4, new QTableWidgetItem(e.fault ? "Yes" : "No"));
+    }
 }
